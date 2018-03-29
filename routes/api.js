c687aa07a598d7b8176701380a3901da9c77e563
newegg <newegg@MBP-Barry.buyabs.corp>
2018-03-29 13:36:30 +0800

    update pay

--- routes/api.js
+++ routes/api.js
@@ -91,6 +91,7 @@ router.post('/updateClock', function(req, res, next) {
 
 
 router.post('/createOrder', function(req, res, next) {
+    console.log('/createOrder', req.session.user)
     if (req.session && req.session.user && req.session.user.openId) {
         dbHandler.createOrder(req, res, req.session.user.openId);
     } else {
